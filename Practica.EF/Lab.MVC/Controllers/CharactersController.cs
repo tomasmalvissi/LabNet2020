9f04de795514f2ad8af16d43c33f6cac7f170908
Tomás Malvissi <50890384+tomasmalvissi@users.noreply.github.com>
2020-11-15 21:57:21 -0300

    add view controller y model de api "The Office"

---
+++ Practica.EF/Lab.MVC/Controllers/CharactersController.cs
@@ -0,0 +1,45 @@
+﻿using Lab.MVC.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab.MVC.Controllers
+{
+    public class CharactersController : Controller
+    {
+        // GET: Characters
+        //public async Task<ActionResult> Index()
+        //{
+        //    var JSON = new WebClient().DownloadString("https://officeapi.dev/api/characters/5e93b4f03af44260882e33b1");
+        //    var chars = JsonConvert.DeserializeObject<Character>(JSON);
+
+        //    return View(chars);
+        //}
+        public async Task<ActionResult> Index()
+        {
+            List<Character> chars = new List<Character>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://officeapi.dev/api/characters/random");
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = await client.GetAsync("5e93b4f03af44260882e33b1");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var rta = await response.Content.ReadAsStreamAsync(chars);
+                    return View(rta);
+                }                
+            }
+            return View();
+        }
+
+    }
+}
---
+++ Practica.EF/Lab.MVC/Models/Character.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab.MVC.Models
+{
+    public class Character
+    {
+        public string _id { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public int __v { get; set; }
+    }
+}
