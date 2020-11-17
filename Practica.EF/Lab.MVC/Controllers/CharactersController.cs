9ab6ad6bbf1deb9d50578a6e5d74338a97f91ccc
Tomás Malvissi <50890384+tomasmalvissi@users.noreply.github.com>
2020-11-16 21:42:29 -0300

    cuando deserializo el objeto json, lo asigna  null a la variable

--- Practica.EF/Lab.MVC/Controllers/CharactersController.cs
+++ Practica.EF/Lab.MVC/Controllers/CharactersController.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -15,31 +16,39 @@ namespace Lab.MVC.Controllers
     public class CharactersController : Controller
     {
         // GET: Characters
-        //public async Task<ActionResult> Index()
-        //{
-        //    var JSON = new WebClient().DownloadString("https://officeapi.dev/api/characters/5e93b4f03af44260882e33b1");
-        //    var chars = JsonConvert.DeserializeObject<Character>(JSON);
-
-        //    return View(chars);
-        //}
         public async Task<ActionResult> Index()
         {
-            List<Character> chars = new List<Character>();
-            using (var client = new HttpClient())
+            var httpClient = new HttpClient();
+            var characters = new List<Character>();
+            string url = "https://www.officeapi.dev/api/characters/";
+            string id = "5e93b4a43af44260882e33b"; 
+            for (int i = 0; i < 10; i++)
             {
-                client.BaseAddress = new Uri("https://officeapi.dev/api/characters/random");
-                client.DefaultRequestHeaders.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                var response = await client.GetAsync("5e93b4f03af44260882e33b1");
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var rta = await response.Content.ReadAsStreamAsync(chars);
-                    return View(rta);
-                }                
+                var json = await httpClient.GetStringAsync(url + "id" + "i");
+                var character = JsonConvert.DeserializeObject<Character>(json);
+                characters.Add(character);
             }
-            return View();
+            return View(characters);
         }
+
+        //public async Task<ActionResult> Index()
+        //{
+        //    List<Character> chars = new List<Character>();
+        //    using (var client = new HttpClient())
+        //    {
+        //        client.BaseAddress = new Uri("https://officeapi.dev/api/characters/random");
+        //        client.DefaultRequestHeaders.Clear();
+        //        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        //        var response = await client.GetAsync("5e93b4f03af44260882e33b1");
+
+        //        if (response.IsSuccessStatusCode)
+        //        {
+        //            var rta = await response.Content.ReadAsStreamAsync(chars);
+        //            return View(rta);
+        //        }                
+        //    }
+        //    return View();
+        //}
 
     }
 }
